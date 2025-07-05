2cc27be657f3b0f1f7064d47365c27ece2d3ffe7
luisgs7 <luisgsilva270@gmail.com>
2025-07-05 14:59:27 -0300

    aula 217

---
+++ lib/components/product_item.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  const ProductItem(this.product, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite),
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.shopping_cart),
+          ),
+        ),
+      ),
+    );
+  }
+}
