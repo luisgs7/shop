b587aa0b931c92ba8f4e3c0c6d4738edb36656da
luisgs7 <luisgsilva270@gmail.com>
2025-07-17 21:15:34 -0300

    new component product_grid

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,37 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/models/product.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/models/product_list.dart';
+import 'package:shop/components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
-
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          title: Text(
-            'Minha Loja',
-            style: TextStyle(
-              color: Colors.white,
-            ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(
+          'Minha Loja',
+          style: TextStyle(
+            color: Colors.white,
           ),
         ),
-        body: GridView.builder(
-          padding: const EdgeInsets.all(10),
-          itemCount: loadedProducts.length,
-          itemBuilder: (ctx, i) => ProductItem(loadedProducts[i]),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
-        ));
+      ),
+      body: ProductGrid(),
+    );
   }
 }
