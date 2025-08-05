ff772db00963b8a48faab6980922843a67cccbb4
luisgs7 <luisgsilva270@gmail.com>
2025-08-04 22:07:47 -0300

    aula 239

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/components/cart_item.dart';
 import 'package:shop/models/cart.dart';
 
 class CartPage extends StatelessWidget {
@@ -8,6 +9,7 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Cart cart = Provider.of<Cart>(context);
+    final items = cart.items.values.toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -40,12 +42,22 @@ class CartPage extends StatelessWidget {
                     ),
                   ),
                   const Spacer(),
-                  TextButton(onPressed: (){}, child: const Text('Comprar'))
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Comprar'),
+                  )
                 ],
               ),
             ),
           ),
-          
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (ctx, i) => CartItemWidget(
+                cartItem: items[i],
+              ),
+            ),
+          ),
         ],
       ),
     );
