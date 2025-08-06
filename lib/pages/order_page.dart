0d5004b16bb927c6ab41e1fa0fb90ab6ad1179c6
luisgs7 <luisgsilva270@gmail.com>
2025-08-05 22:27:53 -0300

    aula 244

---
+++ lib/pages/order_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class OrdersPage extends StatelessWidget {
+  const OrdersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Colors.white,
+        title: const Text('Meus Pedidos'),
+      ),
+    );
+  }
+}
