2c670830a0b1f7ec1d2bfb2042daff621e01a15c
luisgs7 <luisgsilva270@gmail.com>
2025-08-09 21:53:48 -0300

    aula 259

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -9,40 +9,117 @@ class ProductFormPage extends StatefulWidget {
 
 class _ProductFormPageState extends State<ProductFormPage> {
   final priceFocus = FocusNode();
+  final descriptionFocus = FocusNode();
+
+  final _imageUrlFocus = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocus.addListener(updateImage);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    priceFocus.dispose();
+    descriptionFocus.dispose();
+    
+    _imageUrlController.removeListener(updateImage);
+    _imageUrlFocus.dispose();
+  }
+
+  void updateImage() {
+    print("updateImage...");
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Formulário de Produto'),
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        foregroundColor: Colors.white,
-      ),
-      body: Padding(padding: const EdgeInsets.all(15.0), child: Form(
-        child: ListView(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Nome',
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey),
-              )),
-              textInputAction: TextInputAction.next,
-              onFieldSubmitted: (_) {
-                FocusScope.of(context).requestFocus(priceFocus);
-              }
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Preço',
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey),
-              )),
-              focusNode: priceFocus,
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              textInputAction: TextInputAction.next,
-            )
-          ],
+        appBar: AppBar(
+          title: const Text('Formulário de Produto'),
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          foregroundColor: Colors.white,
         ),
-      ),)
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Form(
+            child: ListView(
+              children: [
+                TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Nome',
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        )),
+                    textInputAction: TextInputAction.next,
+                    onFieldSubmitted: (_) {
+                      FocusScope.of(context).requestFocus(priceFocus);
+                    }),
+                TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Preço',
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        )),
+                    focusNode: priceFocus,
+                    keyboardType:
+                        TextInputType.numberWithOptions(decimal: true),
+                    textInputAction: TextInputAction.next,
+                    onFieldSubmitted: (_) {
+                      FocusScope.of(context).requestFocus(descriptionFocus);
+                    }),
+                TextFormField(
+                  decoration: InputDecoration(
+                      labelText: 'Descrição',
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      )),
+                  focusNode: descriptionFocus,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 3,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            labelText: 'URL da Imagem',
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey),
+                            )),
+                        focusNode: _imageUrlFocus,
+                        keyboardType: TextInputType.url,
+                        textInputAction: TextInputAction.done,
+                        onFieldSubmitted: (_) {
+                          FocusScope.of(context).unfocus();
+                        },
+                        controller: _imageUrlController,
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      margin: const EdgeInsets.only(top: 10, left: 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 1),
+                      ),
+                      alignment: Alignment.center,
+                      child: _imageUrlController.text.isEmpty
+                      ? const Text('Informe a URL')
+                      : FittedBox(
+                        child: Image.network(_imageUrlController.text),
+                        fit: BoxFit.cover,
+                      )
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
