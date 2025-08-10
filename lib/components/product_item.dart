3bdf4436f2ba5d09e3a955bfdcfa70b962beab31
luisgs7 <luisgsilva270@gmail.com>
2025-08-10 16:29:00 -0300

    aula 267

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
@@ -21,13 +23,38 @@ class ProductItem extends StatelessWidget {
               icon: const Icon(Icons.edit),
               color: Theme.of(context).primaryColor,
               onPressed: () {
-                Navigator.of(context).pushNamed(AppRoutes.PRODUCT_FORM, arguments: product);
+                Navigator.of(context)
+                    .pushNamed(AppRoutes.PRODUCT_FORM, arguments: product);
               },
             ),
             IconButton(
               icon: const Icon(Icons.delete),
               color: Theme.of(context).colorScheme.error,
-              onPressed: () {},
+              onPressed: () {
+                showDialog<bool>(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                          title: Text('Excluir Produto'),
+                          content: Text('Tem certeza?'),
+                          actions: [
+                            TextButton(
+                              child: Text('Não'),
+                              onPressed: () => Navigator.of(ctx).pop(false),
+                            ),
+                            TextButton(
+                              child: Text('Sim'),
+                              onPressed: () => Navigator.of(ctx).pop(true),
+                            )
+                          ],
+                        )).then((value) {
+                  if (value ?? false) {
+                    Provider.of<ProductList>(
+                      context,
+                      listen: false,
+                    ).removeProduct(product);
+                  }
+                });
+              },
             ),
           ],
         ),
