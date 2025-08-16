52cbfabf25f7089de203948eed84447f87b1c0f7
luisgs7 <luisgsilva270@gmail.com>
2025-08-16 16:09:37 -0300

    aula 297

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AuthPage extends StatelessWidget {
@@ -14,14 +16,42 @@ class AuthPage extends StatelessWidget {
                 colors: [
                   Color.fromRGBO(215, 117, 255, 0.5),
                   Color.fromRGBO(255, 188, 117, 0.9),
-                ]
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
             ),
           ),
           Container(
             width: double.infinity,
             child: Column(
-              children: [],
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 10),
+                  // .. cascade operator
+                  transform: Matrix4.rotationZ(-8* pi/180)..translate(-10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.deepOrange.shade900,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 8,
+                        color: Colors.black26,
+                        offset: Offset(0, 2),
+                      )
+                    ]
+                  ),
+                  child: Text('Minha Loja',
+                  style: TextStyle(
+                    fontSize: 45,
+                    fontFamily: 'Anton',
+                    color: Colors.white
+                  ),
+                  ),
+                )
+              ],
             ),
           )
         ],
