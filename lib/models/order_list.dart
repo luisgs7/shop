12890f0752420ffc1001a07be97f8f514ea1f559
luisgs7 <luisgsilva270@gmail.com>
2025-08-16 12:08:31 -0300

    aula 290

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item.dart';
 import 'package:shop/models/order.dart';
 import 'package:shop/utils/constants.dart';
 
@@ -17,6 +18,37 @@ class OrderList with ChangeNotifier {
   int get itemsCount {
     return _items.length;
   }
+
+  Future<void> loadOrders() async {
+    _items.clear();
+
+    final response = await http.get(
+      Uri.parse('${Constants.ORDER_BASE_URL}.json')
+    );
+    if (response.body == 'null') return;
+
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((orderId, orderData) {
+      _items.add(
+        Order(
+          id: orderId,
+          dateTime: DateTime.parse(orderData['date']),
+          total: orderData['total'],
+          products: (orderData['products'] as List<dynamic>).map((item) {
+            return CartItem(
+            id: item['id'],
+            productId: item['productId'],
+            name: item['name'],
+            quantity: item['quantity'],
+            price: item['price'],
+          );
+        }).toList(),
+        ),
+      );
+    });
+    notifyListeners();
+  }
+
 
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
